0c5876c2823852077de2d5d1f0d289917e4ce81e
schultzRr <rosas_schultz@hotmail.com>
2020-06-10 15:54:31 -0500

    Added Coderia logo to footer

--- wp-content/themes/divi-child/footer.php
+++ wp-content/themes/divi-child/footer.php
@@ -53,15 +53,17 @@ if ( ! is_page_template( 'page-template-blank.php' ) ) : ?>
 
 				<div id="footer-bottom">
 					<div class="container clearfix">
-				<?php
-					if ( false !== et_get_option( 'show_footer_social_icons', true ) ) {
-						get_template_part( 'includes/social_icons', 'footer' );
-					}
-
-					// phpcs:disable WordPress.Security.EscapeOutput.OutputNotEscaped
-					echo et_core_fix_unclosed_html_tags( et_core_esc_previously( et_get_footer_credits() ) );
-					// phpcs:enable
-				?>
+						<?php
+							if ( false !== et_get_option( 'show_footer_social_icons', true ) ) {
+								get_template_part( 'includes/social_icons', 'footer' );
+							}
+						?>
+						<div style="display:flex">
+							<span style="margin-top:3px">powered by</span>
+							<a href="http://coderia.mx" target="_blank">
+								<img src="https://n-bici.com/wp-content/uploads/coderia/coderia_logo.png" style="margin-left:8px; height:27px; width:100px" />
+							</a>
+						</div>
 					</div>	<!-- .container -->
 				</div>
 			</footer> <!-- #main-footer -->
